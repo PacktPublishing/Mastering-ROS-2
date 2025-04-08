160e860c86622748e8ab2170166133fc6305cd3a
jonathan.cacace <jonathan.cacace@eurecat.org>
2025-04-08 12:55:31 +0200

    review code chapter 12

--- Chapter12/mobile_robot_model/launch/display.launch.py
+++ Chapter12/mobile_robot_model/launch/display.launch.py
@@ -1,23 +1,15 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import launch_ros.descriptions
-from launch.substitutions import Command
-from launch.substitutions import PathJoinSubstitution
-from ament_index_python.packages import get_package_share_directory
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch.actions import OpaqueFunction
 import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    
     ld = LaunchDescription()
     
     robot_model_dir = get_package_share_directory('mobile_robot_model')
     urdf_path = os.path.join(robot_model_dir, 'urdf', 'mobile_robot.urdf')
-    urdf = open(urdf_path).read()           
-
+    urdf = open(urdf_path).read()
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -27,6 +19,7 @@ def generate_launch_description():
         parameters=[{'robot_description': urdf}],
     )
 
-    ld.add_action( robot_state_publisher_node )
+    ld.add_action(robot_state_publisher_node)
 
     return ld
+
